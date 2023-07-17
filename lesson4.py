6209b913e8ecc23217e022ee95c7f6b177a92119
skibididopdopdopbrrr <asdfasdfasdfsadf098234502023@gmail.com>
2023-07-17 10:36:44 -0500

    klajsdflkadsadsf

---
+++ lesson4.py
@@ -0,0 +1,37 @@
+# Python booleans:
+print(78 > 56)
+print(10 == 10)
+print(12 < 98)
+print(8 == 6)
+a=12
+b=10
+if a > b:
+    print("GREATER")
+else:
+    print("LESS")
+# bool() functions allows to evaluate any value and in return gives you True or False
+print(bool("Brian"))
+print(bool("88"))
+print(bool("False"))
+print(bool())
+print(bool(0))
+print(bool(None))
+print(bool(""))
+x="Hello"
+y=90
+print(bool(x))
+print(bool(y))
+# Practice:
+if 9 < 5:
+    print("hi")
+else:
+    print("hii")
+print(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool(bool("Hi")))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+a="Variable"
+print(bool(a))
+a="                                                                                                                         Variable"
+print(a)
+print(a.strip())
+a="Python is  so easy."
+print(a.split("  "))
+print(a.replace("so", "very",).replace("  ", " "))
